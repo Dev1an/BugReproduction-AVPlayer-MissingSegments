2baa17eead36e54e63bfb158299a4e6d9cfb0c7b
Damiaan Dufaux <damiaan.dufaux@theoplayer.com>
2023-04-25 11:14:24 +0200

    Initial Commit

---
+++ MissingSegmentsPlayer/MissingSegmentsPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MissingSegmentsPlayerApp.swift
+//  MissingSegmentsPlayer
+//
+//  Created by Damiaan Dufaux on 25/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MissingSegmentsPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
